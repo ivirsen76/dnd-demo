2dc0ee0cba076f96dfcaafe1f0ff7745ac70cbb6
Igor Eremeev <eremeev@3cisd.com>
2020-11-19 20:49:56 -0500

    Add two draggable lists

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import DndWithHandle from "./DndWithHandle.js";
+import DndWithTouch from "./DndWithTouch.js";
+import './index.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div style={{ display: "flex", width: '100%' }}>
+            <div style={{ flexGrow: 1, flexBasis: 0, padding: '1em'}} >
+                <p>With touch</p>
+                <DndWithTouch />
+            </div>
+            <div style={{ flexGrow: 1, flexBasis: 0, padding: '1em'}} >
+                <p>With handle</p>
+                <DndWithHandle />
+            </div>
+        </div>
+    );
 }
 
 export default App;
